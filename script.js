638f9f44c77b539e8cbd8f6ec02f1c848711c530
Kamshat <kamshat.kunbolat@narxoz.kz>
2024-03-25 00:19:28 +0500

    console

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-// document.body.innerHTML = '<h1>Hello bhbdjvb</h1>'
-// console.log('test 2')
+const model = [
+    {type: 'title', value: 'Hello world from 35'},
+    {type: 'text', value: 'here we go with some text'},
+    {type: 'columns', value: [
+        '111111111',
+        '222222222',
+        '333333333'
+    ]}
+]
+
+const $site = document.querySelector('#site')
+
+model.forEach(block => {
+    console.log(block)
+});
